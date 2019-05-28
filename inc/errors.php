11bcfd1b19bf86853a01d58521c6a3c77f252e1d
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-05-28 13:52:47 +0100

    Update errors.php

--- inc/errors.php
+++ inc/errors.php
@@ -1,10 +1,9 @@
 <?php
 
-	// print errors if any
 	if (count($errors) > 0) {
 		foreach ($errors as $error) {
 			echo '<div class="alert alert-danger">';
 			echo '<span>' . $error . '</span>';
 			echo '</div>';
 		}
-	}
+	} 
