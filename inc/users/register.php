fd1b4c6cbc7ef52263ef3eb90e7882f02033e016
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-05-29 18:39:56 +0100

    Add session data once registered

--- inc/users/register.php
+++ inc/users/register.php
@@ -21,7 +21,6 @@
 		$email = mysqli_real_escape_string($db, $_POST['email']);
 		$password = mysqli_real_escape_string($db, $_POST['password']);
 		$password_verify = mysqli_real_escape_string($db, $_POST['password_verify']);
-		$user_perm = "user";
 
 		if (empty($username)) {
 			array_push($errors, "Username is required");
@@ -39,9 +38,31 @@
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
 			$password = md5($password);
+			$user_perm = "user";
+
 			$query = "INSERT INTO users (username, fname, lname, email, password, user_perm) VALUES('$username', '$fname', '$lname', '$email', '$password', '$user_perm')";
 			mysqli_query($db, $query);
+
 			$_SESSION['username'] = $username;
+
+			// get user info from database
+			$user_sql = "SELECT * from users WHERE username='".$_SESSION['username']."'";
+			$result_sql = $db-> query($user_sql);
+
+			// set session data
+			if ($result_sql-> num_rows > 0) {
+				while ($row_sql = $result_sql->fetch_assoc()) {
+					$_SESSION['my_id'] = $row_sql["id"];
+					$_SESSION['my_username'] = $row_sql["username"];
+					$_SESSION['my_fname'] = $row_sql["fname"];
+					$_SESSION['my_lname'] = $row_sql["lname"];
+					$_SESSION['my_email'] = $row_sql["email"];
+					$_SESSION['my_user_perm'] = $row_sql["user_perm"];
+					$_SESSION['my_password'] = $row_sql["password"];
+				}
+			} 
+
 			header('location: ./');
-		} 
+
+		}
 	}
