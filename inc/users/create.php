5d1b3157af9455c6898b4f4193b1709db3a3754c
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-06-03 15:37:34 +0100

    Can now delete users (Has to be admin)

--- inc/users/create.php
+++ inc/users/create.php
@@ -13,52 +13,73 @@
 	// connect database
 	require './inc/config/database.php';
 
-	/**
-	 * create new user
-	 */
-	if (isset($_POST['create_user'])) {
-		$user_perm = mysqli_real_escape_string($db, $_POST['user_perm']);
-		$username = mysqli_real_escape_string($db, $_POST['username']);
-		$fname = mysqli_real_escape_string($db, $_POST['fname']);
-		$lname = mysqli_real_escape_string($db, $_POST['lname']);
-		$email = mysqli_real_escape_string($db, $_POST['email']);
-		$password = mysqli_real_escape_string($db, $_POST['password']);
-		$password_verify = mysqli_real_escape_string($db, $_POST['password_verify']);
+	// start security
+	if ($_SESSION['my_user_perm'] == 'admin') {
 
-		if (empty($user_perm)) {
-			array_push($errors, "User level is required");
-		}
-		if (empty($username)) {
-			array_push($errors, "Username is required");
-		}
-		if (empty($email)) {
-			array_push($errors, "Email is required");
-		}
-		if (empty($password)) {
-			array_push($errors, "Password is required");
+		/**
+		 * create new user
+		 */
+		if (isset($_POST['create_user'])) {
+			$user_perm = mysqli_real_escape_string($db, $_POST['user_perm']);
+			$username = mysqli_real_escape_string($db, $_POST['username']);
+			$fname = mysqli_real_escape_string($db, $_POST['fname']);
+			$lname = mysqli_real_escape_string($db, $_POST['lname']);
+			$email = mysqli_real_escape_string($db, $_POST['email']);
+			$password = mysqli_real_escape_string($db, $_POST['password']);
+			$password_verify = mysqli_real_escape_string($db, $_POST['password_verify']);
+
+			if (empty($user_perm)) {
+				array_push($errors, "User level is required");
+			}
+			if (empty($username)) {
+				array_push($errors, "Username is required");
+			}
+			if (empty($email)) {
+				array_push($errors, "Email is required");
+			}
+			if (empty($password)) {
+				array_push($errors, "Password is required");
+			}
+
+			if ($password != $password_verify) {
+				array_push($errors, "The two passwords do not match");
+			}
+
+			// create user if there are no errors in the form
+			if (count($errors) == 0) {
+				$password = md5($password);
+				$query = "INSERT INTO users (user_perm, username, fname, lname, email, password) VALUES('$user_perm', '$username', '$fname', '$lname', '$email', '$password')";
+				mysqli_query($db, $query);
+				$_SESSION['success'] = '<div class="alert alert-success">
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+				<span>New user created successfully</span>
+				</div>';
+			} else {
+				$_SESSION['error'] = '<div class="alert alert-danger">
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+				<span>Something went wrong</span>
+				</div>';
+			}
 		}
 
-		if ($password != $password_verify) {
-			array_push($errors, "The two passwords do not match");
-		}
-
-		// create user if there are no errors in the form
-		if (count($errors) == 0) {
-			$password = md5($password);
-			$query = "INSERT INTO users (user_perm, username, fname, lname, email, password) VALUES('$user_perm', '$username', '$fname', '$lname', '$email', '$password')";
+		/**
+		 * delete user
+		 */
+		if (isset($_GET['delete'])) {
+			$id = $_GET['delete'];
+			$query = "DELETE FROM users WHERE id='$id'";
 			mysqli_query($db, $query);
 			$_SESSION['success'] = '<div class="alert alert-success">
 			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 			<span aria-hidden="true">&times;</span>
 			</button>
-			<span>New user created successfully</span>
-			</div>';
-		} else {
-			$_SESSION['error'] = '<div class="alert alert-danger">
-			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-			</button>
-			<span>Something went wrong</span>
+			<span>User deleted successfully</span>
 			</div>';
 		}
-	}
+
+	// end security
+} 
