c25c54cf93bb08eb1b983cadef748f7ecbaa3f9e
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-06-04 09:42:22 +0100

    Updates

--- header.php
+++ header.php
@@ -23,60 +23,52 @@
   </head>
   <body>
 
-    <div class="row no-gutters">
+    <header class="header">
+      <div class="container-fluid">
 
-      <div class="col-auto fixed_sidebar">
-        
+        <div class="d-flex justify-content-between align-items-center">
+
+          <span class="d-flex align-items-center">
+            <a href="./" class="brand">
+              <img src="images/logo.png" alt="<?php echo $siteTitle; ?>">
+            </a>
+
+            <?php if (isset($_SESSION["my_id"])) : ?>
+            <ul class="header_menu_left">
+              <?php if ($_SESSION['my_user_perm'] == 'admin' || $_SESSION['my_user_perm'] == 'tech' ) :
+                echo '<li><a href="./">Dashboard</a></li>';
+              else :
+              endif
+              ?>
+              <li><a href="new-request.php">New Request</a></li>
+              <li><a href="my-requests.php">My Requests</a></li>
+            </ul>
+            <?php else :
+              echo $siteTitle;
+            endif ?>
+          </span>
+
+            <?php
+            if (isset($_SESSION["my_id"])) : ?>
+              <ul class="header_menu_right">
+                <li style="font-weight: bold;"><?php echo $_SESSION["my_fname"] . " " . $_SESSION["my_lname"]; ?></li>
+                <li><a href="profile.php">Profile</a></li>
+                  <?php
+                  if ($_SESSION['my_user_perm'] == 'admin') :
+                    echo '<li><a href="settings.php">Settings</a></li>';
+                  else :endif
+                  ?>
+                <li><a href="index.php?logout" class="btn btn-secondary">Logout</a></li>
+              </ul>
+            <?php else : ?>
+              <ul class="header_menu_right">
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php" class="btn btn-secondary">Register</a></li>
+              </ul>
+            <?php endif ?>
+
+        </div>
+
       </div>
 
-      <div class="col site_content">
-
-        <header class="header">
-          <div class="container-fluid">
-
-            <div class="d-flex justify-content-between align-items-center">
-
-              <span class="d-flex align-items-center">
-                <a href="./" class="brand">
-                  <img src="images/logo.png" alt="<?php echo $siteTitle; ?>">
-                </a>
-
-                <?php if (isset($_SESSION["my_id"])) : ?>
-                <ul class="header_menu_left">
-                  <?php if ($_SESSION['my_user_perm'] == 'admin' || $_SESSION['my_user_perm'] == 'tech' ) :
-                    echo '<li><a href="./">Dashboard</a></li>';
-                  else :
-                  endif
-                  ?>
-                  <li><a href="new-request.php">New Request</a></li>
-                  <li><a href="my-requests.php">My Requests</a></li>
-                </ul>
-                <?php else :
-                  echo $siteTitle;
-                endif ?>
-              </span>
-
-                <?php
-                if (isset($_SESSION["my_id"])) : ?>
-                  <ul class="header_menu_right">
-                    <li style="font-weight: bold;"><?php echo $_SESSION["my_fname"] . " " . $_SESSION["my_lname"]; ?></li>
-                    <li><a href="profile.php">Profile</a></li>
-                      <?php
-                      if ($_SESSION['my_user_perm'] == 'admin') :
-                        echo '<li><a href="settings.php">Settings</a></li>';
-                      else :endif
-                      ?>
-                    <li><a href="index.php?logout" class="btn btn-secondary">Logout</a></li>
-                  </ul>
-                <?php else : ?>
-                  <ul class="header_menu_right">
-                    <li><a href="login.php">Login</a></li>
-                    <li><a href="register.php" class="btn btn-secondary">Register</a></li>
-                  </ul>
-                <?php endif ?>
-
-            </div>
-
-          </div>
-
-        </header>
+    </header>
