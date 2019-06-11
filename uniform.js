29e2a54b4cefd00ff794ad8b98a13f5d1eafba46
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-06-11 10:06:39 +0100

    Bug fix

--- uniform.js
+++ uniform.js
@@ -1,14 +1,18 @@
 // tabs
 function openTab(evt, tabId) {
+
   var i, tabContent, tabLinks;
+
   tabContent = document.getElementsByClassName("tab-content");
   for (i = 0; i < tabContent.length; i++) {
     tabContent[i].style.display = "none";
   }
-  tabLinks = document.getElementsByClassName("tab-button");
+
+  tabLinks = document.getElementsByClassName("tab-link");
   for (i = 0; i < tabLinks.length; i++) {
     tabLinks[i].className = tabLinks[i].className.replace(" active", "");
   }
+
   document.getElementById(tabId).style.display = "block";
   evt.currentTarget.className += " active";
 }
